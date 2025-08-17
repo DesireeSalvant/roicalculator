292ae9d20cdc95a184dc106251dc1ea9c07ec217
Desiree' Salvant <desisalvant@gmail.com>
2025-08-17 00:22:36 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ROICalculator from './components/ROICalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ROICalculator />
+    </div>
+  );
+}
+
+export default App;
